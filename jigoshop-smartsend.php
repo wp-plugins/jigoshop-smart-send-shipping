6a3d783ed30e66b39bb81cdbe98285d43c734bed
sp4cecat <sp4cecat@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-01-23 14:31:38 +0000

    Set version number in main plugin file to 2.0
    git-svn-id: https://plugins.svn.wordpress.org/jigoshop-smart-send-shipping/trunk@657403 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- jigoshop-smartsend.php
+++ jigoshop-smartsend.php
@@ -3,7 +3,7 @@
 	Plugin Name: Jigoshop - Smart Send Shipping Plugin
 	Plugin URI: http://codexmedia.com.au/jigoshop-smart-send-shipping-plugin/
 	Description: Add Smart Send shipping calculations to Jigoshop
-	Version: 1.52
+	Version: 2.0
 	Author:  Paul Appleyard	
 	Author URI: http://codexmedia.com.au/
 	License: GPL v2 - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
